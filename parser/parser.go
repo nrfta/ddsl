9ac0d093aba4e098559c06cc069649d32bd36442
Jason White <jwhite@neighborly.com>
2019-04-28 22:07:26 -0400

    added git version option

--- parser/parser.go
+++ parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"bufio"
+	"errors"
 	"github.com/alecthomas/participle"
 	"github.com/alecthomas/participle/lexer"
 	"strings"
@@ -17,13 +18,13 @@ type DDSL struct {
 
 // Command contains details of a create or drop command. Only one of the members will be `true` or populated.
 type Command struct {
-	Database       bool                `@"DATABASE"`
-	Roles          bool                `| @"ROLES"`
-	Extensions     bool                `| @"EXTENSIONS"`
-	ForeignKeys    bool                `| @("FOREIGN" "KEYS")`
-	Schema         string              `| "SCHEMA" @Ident`
-	TablesInSchema string              `| "TABLES" "IN" @Ident`
-	ViewsInSchema  string              `| "VIEWS" "IN" @Ident`
+	Database       *Tag                `"DATABASE" [@@]`
+	Roles          *Tag                `| "ROLES" [@@]`
+	Extensions     *Tag                `| "EXTENSIONS" [@@]`
+	ForeignKeys    *Tag                `| ("FOREIGN" "KEYS") [@@]`
+	Schema         *Name               `| "SCHEMA" @@`
+	TablesInSchema *Name               `| "TABLES" "IN" @@`
+	ViewsInSchema  *Name               `| "VIEWS" "IN" @@`
 	Table          *TableOrViewSubject `| "TABLE" @@`
 	View           *TableOrViewSubject `| "VIEW" @@`
 	Indexes        *SchemaItem         `| "INDEXES" "ON" @@`
@@ -34,13 +35,25 @@ type Command struct {
 type TableOrViewSubject struct {
 	TableOrView string `@Ident`
 	Schema      string `"IN" @Ident`
+	Tag         *Tag   `[@@]`
 }
 
 // SchemaItem contains the schema and table or view when it is the object of the command.
 type SchemaItem struct {
 	Item        string `@SchemaItem`
+	Tag         *Tag   `[@@]`
 	TableOrView string
 	Schema      string
+}
+
+type Name struct {
+	Name string `@Ident`
+	Tag  *Tag   `[@@]`
+}
+
+// Tag contains the git tag to run the DDSL command against.
+type Tag struct {
+	Tag string `@Tag`
 }
 
 // Migrate contains the migration command. Only one of the members will be `true` or nonzero.
@@ -54,9 +67,11 @@ type Migrate struct {
 var (
 	re = `(\s+)` +
 		`|(?P<Keyword>(?i)CREATE|DROP|DATABASE|ROLES|EXTENSIONS|FOREIGN|KEYS|SCHEMA|TABLES|TABLE|VIEWS|VIEW|INDEXES|CONSTRAINTS|IN|ON|MIGRATE|TOP|BOTTOM|UP|DOWN|SQL)` +
-		"|(?P<Sql>(?s)`(.|\\n)*`)" +
+		// TODO: make schema optional and just have term after the dot (public schema)
 		`|(?P<SchemaItem>[a-zA-Z_][a-zA-Z0-9_]*\.[a-zA-Z_][a-zA-Z0-9_]*)` +
 		`|(?P<Ident>[a-zA-Z_][a-zA-Z0-9_]*)` +
+		"|(?P<Sql>(?s)`(.|\\n)*`)" +
+		`|(?P<Tag>@[a-zA-Z0-9_\-\.]*)` +
 		`|(?P<Int>\d*)`
 
 	ddsllLexer = lexer.Must(lexer.Regexp(re))
@@ -65,42 +80,53 @@ var (
 		&DDSL{},
 		participle.Lexer(ddsllLexer),
 		participle.CaseInsensitive("Keyword"),
-		// participle.Elide("Comment"),
-		// Need to solve left recursion detection first, if possible.
-		// participle.UseLookahead(),
 	)
 )
 
 // Parse parses an input of one or more commands and returns a slice of parse trees.
-func Parse(command string) ([]*DDSL, error) {
-	scanner := bufio.NewScanner(strings.NewReader(command))
-	trees := []*DDSL{}
-	inMultiline := false
+func Parse(commands string) ([]*DDSL, error) {
+	scanner := bufio.NewScanner(strings.NewReader(commands))
+	var trees []*DDSL
 	multiline := ""
+	command := ""
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
 		if len(line) == 0 {
 			continue
 		}
-		if inMultiline {
+
+		if len(multiline) > 0 {
 			multiline += "\n" + line
+			// We're in multiline so another backtick signals end of command
 			if strings.Contains(line, "`") {
-				inMultiline = false
-				tree, err := parse(multiline)
-				if err != nil {
-					return nil, err
-				}
-				trees = append(trees, tree)
+				command = multiline
+				multiline = ""
 			}
-		} else if strings.Contains(line, "`") {
-			multiline = line
-			inMultiline = true
 		} else {
-			tree, err := parse(line)
+			countTicks := strings.Count(line, "`")
+			switch {
+
+			// Zero or 2 backticks on a line means command is a one-liner
+			case countTicks == 0 || countTicks == 2:
+				command = line
+
+			// Single backtick means enter multiline mode
+			case countTicks == 1:
+				multiline = line
+
+			default:
+				return nil, errors.New("syntax error: too many backticks on a line")
+			}
+		}
+
+		// Once command is complete, parse it
+		if len(command) > 0 {
+			tree, err := parse(command)
 			if err != nil {
 				return nil, err
 			}
 			trees = append(trees, tree)
+			command = ""
 		}
 	}
 
@@ -111,6 +137,7 @@ func parse(command string) (*DDSL, error) {
 	tree := &DDSL{}
 	err := ddsllParser.ParseString(command, tree)
 	if err == nil {
+		// Some commands require a bit of touch up
 		if len(tree.Sql) > 0 {
 			tree.Sql = strings.Trim(tree.Sql, "`")
 		}
@@ -131,6 +158,7 @@ func parse(command string) (*DDSL, error) {
 
 }
 
+// Split `schema.table_or_view` in to schema and table_or_view fields
 func (si *SchemaItem) populate() {
 	if len(si.Item) > 0 {
 		parts := strings.Split(si.Item, ".")
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,44 +1,45 @@
 package parser_test
 
 import (
-	"github.com/neighborly/ddsl/parser"
-	"github.com/alecthomas/repr"
-	"testing"
 	"fmt"
+	"github.com/alecthomas/repr"
+	"github.com/neighborly/ddsl/parser"
+	"testing"
 )
 
 func TestParser(t *testing.T) {
 	command := `
 		create DaTaBaSe
-		create roles
+		create roles @v1.3
 		create extensions
-		create foreign keys
+		create foreign keys @v1.3
 		create schema foo
-		create tables in foo
+		create tables in foo @v1.3
 		create views in foo
-		create table bar in foo
+		create table bar in foo @v1.3
 		create view cat in foo
-		create indexes on foo.bar
+		create indexes on foo.bar @v1.3
 		create constraints on foo.cat
-		drop database
+		drop database @v1.3
 		drop roles
-		drop extensions
+		drop extensions @v1.3
 		drop foreign keys
-		drop schema foo
+		drop schema foo @v1.3
 		drop tables in foo
-		drop views in foo
+		drop views in foo @v1.3
 		drop table bar in foo
-		drop view cat in foo
+		drop view cat in foo @v1.3
 		drop indexes on foo.bar
-		drop constraints on foo.cat
+		drop constraints on foo.cat @v1.3
 		migrate top
 		migrate bottom
 		migrate up 2
 		migrate down 2
 	`
-	command += "sql `UPDATE foo.bar SET field = 3;\n" +
+	command += "sql `UPDATE foo.bar SET field = 3;`\n" +
 		"" +
-		"DELETE FROM foo.bar WHERE field = 5;\n" +
+		"sql `DELETE FROM foo.bar WHERE field = 5;\n" +
+		"INSERT INTO foo.bar (field) VALUES (5);`" +
 		"`"
 
 	trees, err := parser.Parse(command)
