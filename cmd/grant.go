17acc2bb2684ff5844db683535170c92000b293a
Jason White <jwhite@neighborly.com>
2019-09-06 09:55:27 -0400

    added functions, procedures, triggers commands

--- cmd/grant.go
+++ cmd/grant.go
@@ -11,18 +11,21 @@ import (
 // grantCmd represents the grant command
 var grantCmd = &cobra.Command{
 	Use:   "grant",
+	Short: parser.ShortDesc("grant"),
+	Long: `Grant or revoke privileges on database objects`,
 }
 
 var grantPrivilegesCmd = &cobra.Command{
 	Use: "privileges",
+	Short: parser.ShortDesc("grant privileges"),
+	Long: `Grant or revoke privileges on database objects`,
 	Aliases: []string{"privs"},
 }
 
 var grantPrivilegesOnCmd = &cobra.Command{
 	Use: "on",
 	Short: parser.ShortDesc("grant privileges on"),
-	Long: `
-`,
+	Long: `Grant or revoke privileges on database objects`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("additional arguments required, use -h for help")
 		os.Exit(1)
@@ -31,6 +34,7 @@ var grantPrivilegesOnCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(grantCmd)
+	grantCmd.AddCommand(grantPrivilegesCmd)
 	grantPrivilegesCmd.AddCommand(grantPrivilegesOnCmd)
 	grantPrivilegesOnCmd.AddCommand(grantDatabaseCmd)
 	grantPrivilegesOnCmd.AddCommand(grantSchemaCmd)
--- cmd/revoke.go
+++ cmd/revoke.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/neighborly/ddsl/parser"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -10,13 +11,8 @@ import (
 // revokeCmd represents the revoke command
 var revokeCmd = &cobra.Command{
 	Use:   "revoke",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: parser.ShortDesc("revoke"),
+	Long: `Revoke privileges on database objects`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("additional arguments required, use -h for help")
 		os.Exit(1)
@@ -26,5 +22,4 @@ to quickly create a Cobra application.`,
 func init() {
 	rootCmd.AddCommand(revokeCmd)
 	revokeCmd.AddCommand(grantPrivilegesCmd)
-	revokeCmd.AddCommand(grantPrivilegesOnCmd)
 }
